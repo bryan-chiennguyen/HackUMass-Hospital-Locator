ca3e86ebad8e6e0502f79cf7fdbf1123ef2b1cb2
bryan-chiennguyen <bryan.chiennguyen@gmail.com>
2020-12-20 12:27:57 +0700

    Second change

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,20 +1,38 @@
 <script>
 	import Navbar from './Navbar.svelte';
-	export let name;
+	import Map from './map.svelte'
+	//export let name;
+	export let ready;
 </script>
 
-<main>
-	<Navbar/>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<svelte:head>
+	<script defer async
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCo3l2gDAT_SeGV20UtefK_ERRZ_tHygS0
+	&callback=initMap">
+	</script>
+</svelte:head>
+
+<main id="content">
+	<div id="left">
+		<Navbar/>
+	</div>
+	<div id="right">
+		{ #if ready }
+		<Map/>
+		{ /if } 
+	</div>
 </main>
 
 <style>
+	:global(body) {
+		background-color:beige;
+	}
+	
 	main {
-		text-align: center;
+		/*text-align: center;*/
 		padding: 1em;
 		max-width: 240px;
-		margin: 0 auto;
+		/*margin: 0 auto;*/
 	}
 
 	h1 {
@@ -27,5 +45,26 @@
 		main {
 			max-width: none;
 		}
+	}
+
+	#content {
+		/*border: 1px solid black;*/
+	}
+
+	#content > div {
+		height: 100px;
+		box-sizing: border-box;
+		-moz-box-sizing: border-box;
+	}
+
+	#left {
+		float: left;
+		width: 30%;
+		border-right: 1px solid black;
+	}
+
+	#right {
+		float: right;
+		width: 70%;
 	}
 </style>
--- svelte-app/src/Navbar.svelte
+++ svelte-app/src/Navbar.svelte
@@ -1,1 +1,17 @@
-<h1>This will the navbar</h1>
+<h1>Hospital Locator</h1>
+
+<p>Description of our website</p>
+
+<button>Find My Nearest Hospital</button>
+
+<style>
+    h1 {
+
+    }
+
+    button {
+        color:crimson;
+        background-color:cornflowerblue;
+    }
+
+</style>
